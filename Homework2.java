7f717e36da59c8abb210e785a11b99a3ab563550
sumeyyeisen <2212101035@ogr.mehmetakif.edu.tr>
2022-11-08 22:05:43 +0300

    Create Homework2.java

---
+++ Homework2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sumey
+ */
+public class Example6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int F=1 ;
+        int S=20 ;
+        while(S>=1) {
+            S=S-3;
+            F=F+S;
+            F=F+2; }
+        System.out.println("F:"+F);
+            
+        
+    }
+    
+}
